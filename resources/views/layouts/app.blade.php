c4e6868317ea618b9e9b71b49fccad7bc19f23cd
AlbertWebs <albertmuhatia@gmail.com>
2025-11-13 15:37:44 +0300

    installable

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,11 +3,14 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="theme-color" content="#0f172a">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Spare Parts POS') }} - @yield('title', 'Dashboard')</title>
     
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600,700" rel="stylesheet" />
+    <link rel="manifest" href="{{ asset('manifest.webmanifest') }}">
+    <link rel="icon" type="image/svg+xml" href="{{ asset('icons/app-icon.svg') }}">
     
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
